54472e1ae029678da4861c1227e6623d01426978
Alan Vergara Jackson <115580136+alanvergarajackson@users.noreply.github.com>
2023-01-18 23:20:29 -0300

    diferencia al escribir variable mas limpia

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -2,6 +2,7 @@
 @section('title','Cursos '.$id)
 
 @section('content')
-<h2>bienvenido al curso de: <?php echo $id;?></h2>
+<!--<h2>bienvenido al curso de: <?php //echo $id;?></h2>-->
+<h2>bienvenido al curso de: {{$id}} </h2>
 @endsection
 
