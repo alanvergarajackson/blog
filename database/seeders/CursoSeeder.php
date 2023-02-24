bfc352201bbbc228b807cc1357ff2f8147bd64fc
Alan Vergara Jackson <115580136+alanvergarajackson@users.noreply.github.com>
2023-02-23 23:19:51 -0300

    tinker y seeders

---
+++ database/seeders/CursoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Curso;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $curso = new Curso();
+        $curso->name = "laravel";
+        $curso->descripcion = "el mejor curso del mundo";
+        $curso->categoria ="Desarrollo Web";
+        $curso->save();
+
+    }
+}
